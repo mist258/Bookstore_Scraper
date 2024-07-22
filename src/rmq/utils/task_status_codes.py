b4615c643fafbf682715dd38bca99a3f7887ebfa
Stas Polos <polos_ss@groupbwt.com>
2024-07-22 16:13:05 +0300

    feat(rmq): add rmq module

---
+++ src/rmq/utils/task_status_codes.py
@@ -0,0 +1,12 @@
+from enum import IntEnum
+
+
+class TaskStatusCodes(IntEnum):
+    NOT_PROCESSED = 0
+    IN_QUEUE = 1
+    SUCCESS = 2
+    PARTIAL_SUCCESS = 21
+    ERROR = 4
+    HARDWARE_ERROR = 41
+    NOT_FOUND = 404
+    EXPORTED = 22
