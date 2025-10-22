6b61d572d8f1a7294ac0f734f1f9abbdff785da7
mist258 <natalia.kolchuk@gmail.com>
2025-10-22 04:44:53 +0300

    configured script

--- src/parsers/parser_factory.py
+++ src/parsers/parser_factory.py
@@ -9,8 +9,8 @@ from typing import Any, Generic, TypeVar
 
 from scrapy.http import Response
 
-from .exceptions import MultipleParsersFoundError, ParserNotFoundError
-from .response_parser import ResponseParser
+from src.parsers.exceptions import MultipleParsersFoundError, ParserNotFoundError
+from src.parsers.response_parser import ResponseParser
 
 AnyResponseParser = TypeVar("AnyResponseParser", bound=ResponseParser)
 
